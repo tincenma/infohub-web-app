e29d18d1c0bf3bc6844020f94b23d4f09c509cd0
tincenma <temirlanilyasov1@gmail.com>
2025-04-04 20:06:21 +0500

    delete all

--- src/config.ts
+++
@@ -1,4 +0,0 @@
-export const NEWS_API_KEY: string = import.meta.env.VITE_NEWS_API_KEY;
-export const NEWS_API_BASE_URL: string = import.meta.env.VITE_NEWS_API_BASE_URL;
-export const WEATHER_API_KEY: string = import.meta.env.VITE_WEATHER_API_KEY;
-export const WEATHER_API_BASE_URL: string = import.meta.env.VITE_WEATHER_API_BASE_URL;
